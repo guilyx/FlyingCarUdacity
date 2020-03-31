354c06f8fae21ed2549ddb8ef2d95bfe7c6cecb8
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-31 18:47:19 +0200

    🚧 WIP: Voronoi graph based planning

---
+++ lib/voronoi_utils.py
@@ -0,0 +1,152 @@
+from bresenham import bresenham
+from scipy.spatial import Voronoi
+import numpy as np
+from queue import PriorityQueue
+import networkx as nx
+
+def closest_node(graph, current_position):
+    '''
+    Compute the closest node in the graph to the current position
+    '''
+    closest_node = None
+    dist = 100000
+    xy_position = (current_position[0], current_position[1])
+    for p in graph.nodes:
+        d = heuristic(xy_position, p)
+        if d < dist:
+            closest_node = p
+            dist = d
+    return closest_node
+
+def create_grid_and_edges(data, drone_altitude, safety_distance):
+    '''
+    Create a grid representation of a 2D configuration space and a Voronoi Graph
+    '''
+    # minimum and maximum north coordinates
+    north_min = np.floor(np.min(data[:, 0] - data[:, 3]))
+    north_max = np.ceil(np.max(data[:, 0] + data[:, 3]))
+    # minimum and maximum east coordinates
+    east_min = np.floor(np.min(data[:, 1] - data[:, 4]))
+    east_max = np.ceil(np.max(data[:, 1] + data[:, 4]))
+
+    # given the minimum and maximum coordinates we can
+    # calculate the size of the grid.
+    north_size = int(np.ceil(north_max - north_min))
+    east_size = int(np.ceil(east_max - east_min))
+
+    # Initialize an empty grid
+    grid = np.zeros((north_size, east_size))
+    # Initialize an empty list for Voronoi points
+    points = []
+
+    # Populate the grid with obstacles
+    for i in range(data.shape[0]):
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+        if alt + d_alt + safety_distance > drone_altitude:
+            obstacle = [
+                int(np.clip(north - d_north - safety_distance - north_min, 0, north_size-1)),
+                int(np.clip(north + d_north + safety_distance - north_min, 0, north_size-1)),
+                int(np.clip(east - d_east - safety_distance - east_min, 0, east_size-1)),
+                int(np.clip(east + d_east + safety_distance - east_min, 0, east_size-1)),
+            ]
+            grid[obstacle[0]:obstacle[1]+1, obstacle[2]:obstacle[3]+1] = 1
+            # add center of obstacles to points list
+            points.append([north - north_min, east - east_min])
+
+    # create a voronoi graph based on location of obstacle centres
+    graph = Voronoi(points)
+
+    # check each edge from graph.ridge_vertices for collision
+    edges = []
+    for v in graph.ridge_vertices:
+        p1 = graph.vertices[v[0]]
+        p2 = graph.vertices[v[1]]
+        cells = list(bresenham(int(p1[0]), int(p1[1]), int(p2[0]), int(p2[1])))
+        hit = False
+
+        for c in cells:
+            # First check if we're off the map
+            if np.amin(c) < 0 or c[0] >= grid.shape[0] or c[1] >= grid.shape[1]:
+                hit = True
+                break
+            # Next check if we're in collision
+            if grid[c[0], c[1]] == 1:
+                hit = True
+                break
+
+        # If the edge does not hit on obstacle
+        # add it to the list
+        if not hit:
+            # array to tuple for future graph creation step)
+            p1 = (p1[0], p1[1])
+            p2 = (p2[0], p2[1])
+            edges.append((p1, p2))
+
+    return grid, edges, int(north_min), int(east_min)
+
+
+def create_graph(edges):
+    graph = nx.Graph()
+    for elem in edges:
+        p1 = elem[0]
+        p2 = elem[1]
+        dist = heuristic(p1, p2)
+        graph.add_edge(p1, p2, weight=dist)
+    return graph
+
+
+def heuristic(n1, n2):
+    return(np.linalg.norm(np.array(n2) - np.array(n1)))
+
+
+def a_star_graph(graph, start, goal, h):
+    '''
+    A* working with NetworkX graphs
+    '''
+
+    path = []
+    path_cost = 0
+    queue = PriorityQueue()
+    queue.put((0, start))
+    visited = set(start)
+
+    branch = {}
+    found = False
+    
+    while not queue.empty():
+        item = queue.get()
+        current_node = item[1]
+        if current_node == start:
+            current_cost = 0.0
+        else:              
+            current_cost = branch[current_node][0]
+            
+        if current_node == goal:        
+            print('Found a path.')
+            found = True
+            break
+        else:
+            for next_node in graph[current_node]:
+                cost = graph.edges[current_node, next_node]['weight']
+                branch_cost = current_cost + cost
+                queue_cost = branch_cost + h(next_node, goal)
+                
+                if next_node not in visited:                
+                    visited.add(next_node)
+                    branch[next_node] = (branch_cost, current_node)
+                    queue.put((queue_cost, next_node))
+                    
+    if found:
+        # retrace steps
+        n = goal
+        path_cost = branch[n][0]
+        path.append(goal)
+        while branch[n][1] != start:
+            path.append(branch[n][1])
+            n = branch[n][1]
+        path.append(branch[n][1])
+    else:
+        print('**********************')
+        print('Failed to find a path!')
+        print('**********************') 
+    return path[::-1], path_cost
