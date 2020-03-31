3bad56cec851554a3de1d56b9d55113598a925d4
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-31 18:55:12 +0200

    ✨ Voronoi Graphs based planning fully functional

--- motion_planning_voronoi.py
+++ motion_planning_voronoi.py
@@ -83,6 +83,7 @@ class MotionPlanning(Drone):
     def takeoff_transition(self):
         self.flight_state = States.TAKEOFF
         print("takeoff transition")
+        print(self.target_position[2])
         self.takeoff(self.target_position[2])
 
     def waypoint_transition(self):
@@ -164,7 +165,7 @@ class MotionPlanning(Drone):
         path = prunePath(path, epsilon=1e-2)
 
         # Convert path to waypoints
-        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
+        waypoints = [[int(p[0] + north_offset), int(p[1] + east_offset), TARGET_ALTITUDE, 0] for p in path]
         # Set self.waypoints
         self.waypoints = waypoints
         # TODO: send waypoints to sim (this is just for visualization of waypoints)
